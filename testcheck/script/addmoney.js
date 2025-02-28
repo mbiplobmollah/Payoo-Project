722d60de2c9e0bffcc168cdc713fd3b15fd93870
mbiplobmollah <biplobmollah16@gmail.com>
2025-02-28 22:12:26 +0600

    project is done

--- testcheck/script/addmoney.js
+++ testcheck/script/addmoney.js
@@ -6,11 +6,41 @@ document.getElementById('add-money-btn').addEventListener('click', function (eve
     const account = document.getElementById('acc-number').value;
     const accountNumber = getInputValueByID('acc-number');
     const mainBalance = getInnerTextByID('main-balance')
+    const selectedBank = document.getElementById('allbank').value;
     
+
+    if(amount <0){
+        alert('Enter a valid Amount');
+        return;
+    }
+
    if(account.length === 11){
     if(pin === 1234){
       const sum =  mainBalance + amount;
       setInnerTextByIdValue('main-balance',sum)
+
+    const container = document.getElementById('transaction-container');
+
+    // one way of doing transaction;
+    // const p = document.createElement('p');
+    // p.innerText= `
+    // added ${amount} from ${account} account
+    // `
+    // container.appendChild(p);
+        // another way of doing transaction;
+    const div = document.createElement('div');
+    div.classList.add('bg-red-400')
+    div.innerHTML = `
+    <h1 class = "text-yellow-300">Added Money from ${selectedBank}</h1>
+    <h3>${amount}</h3>
+    <p>account number: ${account}</p>
+    `
+    container.appendChild(div);
+
+
+
+
+
     }
     else{
         alert('Need correct PIN')
--- testcheck/script/cashout.js
+++ testcheck/script/cashout.js
@@ -5,12 +5,27 @@ document.getElementById('cash-out-btn').addEventListener('click', function (even
     const pin = getInputValueByID('pin');
     const mainBalance = getInnerTextByID('main-balance');
     
-  
+  if(amount > mainBalance){
+    alert('Invalid Amount');
+    return;
+  }
     if(accountNumber.length === 11){
         if(pin === 1234){
           
             const subtract = mainBalance - amount;
             setInnerTextByIdValue('main-balance', subtract);
+
+            
+    const container = document.getElementById('transaction-container');
+
+    const p = document.createElement('p');
+    p.innerText= `
+    cash-out ${amount} from ${accountNumber} account
+    `
+    container.appendChild(p);
+
+
+
         }
         else{
             alert('PIN not valid');
